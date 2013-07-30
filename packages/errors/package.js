1276bb979ba4dfa9f62ee4e5aca138d77cd46014
Oliver Song <osong@mit.edu>
2013-07-29 21:58:54 -0700

    Tests...janky

--- packages/errors/package.js
+++ packages/errors/package.js
@@ -8,3 +8,10 @@ Package.on_use( function(api, where) {
 
   api.add_files(['errors.coffee', 'errors_list.html', 'errors_list.coffee'], 'client');
 });
+
+Package.on_test(function(api) {
+  api.use('errors', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');
+
+  api.add_files('errors_tests.js', 'client');
+});
